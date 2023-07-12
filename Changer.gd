e30918c9f97b9bc55ca24c307fa6885766e27f4a
Văn Viết Hiếu Anh <vanviethieuanh@gmail.com>
2023-07-13 02:45:45 +0700

    Adapt godot 4
    still lagging

--- Changer.gd
+++ Changer.gd
@@ -1,21 +1,29 @@
 extends ColorRect
 
 signal scene_changed
-@export var next_scene; # (String, FILE, "*.tscn")
+@export var next_scene: String;
+
+var tween: Tween;
 
 func In(duration:float):
 	self.material.set_shader_parameter("position", -1.5)
 	self.material.set_shader_parameter("in_out", 0)
-	$Tween.interpolate_property(self.material, "shader_param/position", -1.5, 1, duration, Tween.TRANS_CUBIC, Tween.EASE_IN)
-	$Tween.start()
 	
+	tween = self.create_tween()
+	tween.tween_method(SetShaderPosition, -1.5, 1, duration)
+	tween.finished
+
 func Out(duration:float):
 	self.material.set_shader_parameter("position", 1)
 	self.material.set_shader_parameter("in_out", 1)
-	$Tween.interpolate_property(self.material, "shader_param/position", 1, -1.5, duration, Tween.TRANS_CUBIC, Tween.EASE_OUT)
-	$Tween.start()
+	
+	tween = self.create_tween()
+	tween.tween_method(SetShaderPosition, 1, -1.5, duration)
+	tween.finished
 	
 	await get_tree().create_timer(duration).timeout
 	assert(get_tree().change_scene_to_file(next_scene) == OK)
 	emit_signal("scene_changed")
-	
+
+func SetShaderPosition(value: float):
+	self.material.set("shader_parameter/position", value)
--- Result.gd
+++ Result.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _ready():
 	$CanvasLayer/Changer.In(1.5)
+
+func _process(delta):
+	if Input.is_action_pressed("change"):
+		$CanvasLayer/Changer.Out(2.0)
