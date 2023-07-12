9c4ff16c26ac529a0e58a3374594eb95c3741cea
Văn Viết Hiếu Anh <vanviethieuanh@gmail.com>
2023-07-13 01:09:32 +0700

    Simple adapt
    Convert to godot 4, not tested

--- Changer.gd
+++ Changer.gd
@@ -1,21 +1,21 @@
 extends ColorRect
 
 signal scene_changed
-export(String, FILE, "*.tscn") var next_scene;
+@export var next_scene; # (String, FILE, "*.tscn")
 
 func In(duration:float):
-	self.material.set_shader_param("position", -1.5)
-	self.material.set_shader_param("in_out", 0)
+	self.material.set_shader_parameter("position", -1.5)
+	self.material.set_shader_parameter("in_out", 0)
 	$Tween.interpolate_property(self.material, "shader_param/position", -1.5, 1, duration, Tween.TRANS_CUBIC, Tween.EASE_IN)
 	$Tween.start()
 	
 func Out(duration:float):
-	self.material.set_shader_param("position", 1)
-	self.material.set_shader_param("in_out", 1)
+	self.material.set_shader_parameter("position", 1)
+	self.material.set_shader_parameter("in_out", 1)
 	$Tween.interpolate_property(self.material, "shader_param/position", 1, -1.5, duration, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 	$Tween.start()
 	
-	yield(get_tree().create_timer(duration), "timeout")
-	assert(get_tree().change_scene(next_scene) == OK)
+	await get_tree().create_timer(duration).timeout
+	assert(get_tree().change_scene_to_file(next_scene) == OK)
 	emit_signal("scene_changed")
 	
